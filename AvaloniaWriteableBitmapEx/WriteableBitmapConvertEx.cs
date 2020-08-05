31d13f7d0c5420816b984d829c46577fd0be8bb1
flabbet <flubbet@gmail.com>
2020-08-05 22:11:41 +0200

    Added FromByteArray

--- AvaloniaWriteableBitmapEx/WriteableBitmapConvertEx.cs
+++ AvaloniaWriteableBitmapEx/WriteableBitmapConvertEx.cs
@@ -31,5 +31,45 @@ namespace AvaloniaWriteableBitmapEx
                 return result;
             }
         }
+
+        /// <summary>
+        /// Copies color information from an ARGB byte array into this WriteableBitmap starting at a specific buffer index.
+        /// </summary>
+        /// <param name="bmp">The WriteableBitmap.</param>
+        /// <param name="offset">The starting index in the buffer.</param>
+        /// <param name="count">The number of bytes to copy from the buffer.</param>
+        /// <param name="buffer">The color buffer as byte ARGB values.</param>
+        /// <returns>The WriteableBitmap that was passed as parameter.</returns>
+        public static WriteableBitmap FromByteArray(this WriteableBitmap bmp, byte[] buffer, int offset, int count)
+        {
+            using (var context = bmp.GetBitmapContext())
+            {
+                BitmapContext.BlockCopy(buffer, offset, context, 0, count);
+                return bmp;
+            }
+        }
+
+        /// <summary>
+        /// Copies color information from an ARGB byte array into this WriteableBitmap.
+        /// </summary>
+        /// <param name="bmp">The WriteableBitmap.</param>
+        /// <param name="count">The number of bytes to copy from the buffer.</param>
+        /// <param name="buffer">The color buffer as byte ARGB values.</param>
+        /// <returns>The WriteableBitmap that was passed as parameter.</returns>
+        public static WriteableBitmap FromByteArray(this WriteableBitmap bmp, byte[] buffer, int count)
+        {
+            return bmp.FromByteArray(buffer, 0, count);
+        }
+
+        /// <summary>
+        /// Copies all the color information from an ARGB byte array into this WriteableBitmap.
+        /// </summary>
+        /// <param name="bmp">The WriteableBitmap.</param>
+        /// <param name="buffer">The color buffer as byte ARGB values.</param>
+        /// <returns>The WriteableBitmap that was passed as parameter.</returns>
+        public static WriteableBitmap FromByteArray(this WriteableBitmap bmp, byte[] buffer)
+        {
+            return bmp.FromByteArray(buffer, 0, buffer.Length);
+        }
     }
 }
